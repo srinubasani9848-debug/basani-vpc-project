85c07485aa81784c0ff752fce243f2beb131f21b
srinubasani9848-debug <srinubasani9848@gmail.com>
2025-10-14 13:10:44 +0530

    Update ec2.tf

--- compute/ec2.tf
+++ compute/ec2.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_security_group" "basani_sg" {
-  name        = "basani-sg"
+resource "aws_security_group" "srinivas_sg" {
+  name        = "srinivas-sg"
   description = "Allow HTTP traffic"
   vpc_id      = var.vpc_id
 
@@ -22,45 +22,33 @@ resource "aws_security_group" "basani_sg" {
   }
 
   tags = {
-    Name    = "basani-sg"
-    Project = "basani"
+    Name    = "srinivas-sg"
+    Project = "srinivas"
   }
 }
 
-resource "aws_instance" "basani_ec2" {
+resource "aws_instance" "srinivas_ec2" {
   ami           = "ami-0c2b8ca1dad447f8a" # Amazon Linux 2 in us-east-1
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
-  vpc_security_group_ids = [aws_security_group.basani_sg.id]  # <-- changed here
+  vpc_security_group_ids = [aws_security_group.srinivas_sg.id]
 
   tags = {
-    Name    = "basani-ec2"
-    Project = "basani"
+    Name    = "srinivas-ec2"
+    Project = "srinivas"
   }
 
   user_data = <<-EOF
             #!/bin/bash
-
-            # Update system packages
             yum update -y
-
-            # Install Apache
             yum install -y httpd
-
-            # Remove default Apache welcome page config
             if [ -f /etc/httpd/conf.d/welcome.conf ]; then
                 rm -f /etc/httpd/conf.d/welcome.conf
             fi
-
-            # Create custom index.html
             echo "Hello DevOps team" > /var/www/html/index.html
-
-            # Ensure Apache starts on boot
             systemctl enable httpd
-
-            # Start or restart Apache to apply changes
             systemctl restart httpd
             EOF
-
 }
+
 
