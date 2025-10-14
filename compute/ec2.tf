8d748a7203893c0d36b6184beb582d01ca0b4f2e
srinubasani9848-debug <srinubasani9848@gmail.com>
2025-10-14 09:06:50 +0530

    Create ec2.tf

---
+++ compute/ec2.tf
@@ -0,0 +1,48 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_security_group" "basani_sg" {
+  name        = "basani-sg"
+  description = "Allow HTTP traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "basani-sg"
+    Project = "basani"
+  }
+}
+
+resource "aws_instance" "basani_ec2" {
+  ami           = "ami-0c2b8ca1dad447f8a" # Amazon Linux 2 in us-east-1
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  security_groups = [aws_security_group.basani_sg.name]
+
+  tags = {
+    Name    = "basani-ec2"
+    Project = "basani"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello DevOps team" > /var/www/html/index.html
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              EOF
+}
