b09c8eeddbca88b0828e02c7e5446f01a5e471b4
srinubasani9848-debug <srinubasani9848@gmail.com>
2025-10-14 13:11:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "basani-terraform-backend-2025"
+    bucket         = "srinivas-terraform-backend-2025"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "basani-tf-lock"
+    dynamodb_table = "srinivas-tf-lock"
     encrypt        = true
   }
 }
+
