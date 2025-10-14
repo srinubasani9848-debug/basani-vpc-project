4bbb9674aa1f474e77ff4e8606317099e13bd232
srinubasani9848-debug <srinubasani9848@gmail.com>
2025-10-14 13:08:31 +0530

    Update vpc.tf

--- network/vpc.tf
+++ network/vpc.tf
@@ -2,48 +2,60 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_vpc" "basani_vpc" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "srinivas_vpc" {
+  cidr_block = var.vpc_cidr
   tags = {
-    Name    = "basani-vpc"
-    Project = "basani"
+    Name    = "srinivas-vpc"
+    Project = "srinivas"
   }
 }
 
-resource "aws_subnet" "basani_subnet" {
-  vpc_id                  = aws_vpc.basani_vpc.id
-  cidr_block              = "10.0.1.0/24"
+resource "aws_subnet" "srinivas_public_subnet" {
+  vpc_id                  = aws_vpc.srinivas_vpc.id
+  cidr_block              = var.public_subnet_cidr
   map_public_ip_on_launch = true
   availability_zone       = "us-east-1a"
   tags = {
-    Name    = "basani-subnet"
-    Project = "basani"
+    Name    = "srinivas-public-subnet"
+    Project = "srinivas"
   }
 }
 
-resource "aws_internet_gateway" "basani_igw" {
-  vpc_id = aws_vpc.basani_vpc.id
+resource "aws_subnet" "srinivas_private_subnet" {
+  vpc_id            = aws_vpc.srinivas_vpc.id
+  cidr_block        = var.private_subnet_cidr
+  map_public_ip_on_launch = false
+  availability_zone = "us-east-1a"
   tags = {
-    Name    = "basani-igw"
-    Project = "basani"
+    Name    = "srinivas-private-subnet"
+    Project = "srinivas"
   }
 }
 
-resource "aws_route_table" "basani_rt" {
-  vpc_id = aws_vpc.basani_vpc.id
+resource "aws_internet_gateway" "srinivas_igw" {
+  vpc_id = aws_vpc.srinivas_vpc.id
   tags = {
-    Name    = "basani-rt"
-    Project = "basani"
+    Name    = "srinivas-igw"
+    Project = "srinivas"
   }
 }
 
-resource "aws_route" "basani_route" {
-  route_table_id         = aws_route_table.basani_rt.id
+resource "aws_route_table" "srinivas_rt" {
+  vpc_id = aws_vpc.srinivas_vpc.id
+  tags = {
+    Name    = "srinivas-rt"
+    Project = "srinivas"
+  }
+}
+
+resource "aws_route" "srinivas_route" {
+  route_table_id         = aws_route_table.srinivas_rt.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.basani_igw.id
+  gateway_id             = aws_internet_gateway.srinivas_igw.id
 }
 
-resource "aws_route_table_association" "basani_rta" {
-  subnet_id      = aws_subnet.basani_subnet.id
-  route_table_id = aws_route_table.basani_rt.id
+resource "aws_route_table_association" "srinivas_rta" {
+  subnet_id      = aws_subnet.srinivas_public_subnet.id
+  route_table_id = aws_route_table.srinivas_rt.id
 }
+
