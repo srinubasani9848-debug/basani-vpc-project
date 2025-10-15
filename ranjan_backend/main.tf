9b6d81c5bd2f5445220c6f4a2567dd2c84facfbb
srinubasani9848-debug <srinubasani9848@gmail.com>
2025-10-15 08:58:44 +0530

    Delete ranjan_backend directory

--- ranjan_backend/main.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "ranjan-terraform-backend-2025"
-    key            = "terraform.tfstate"
-    region         = "us-east-2"
-    dynamodb_table = "ranjan-tf-lock"
-    encrypt        = true
-  }
-}
