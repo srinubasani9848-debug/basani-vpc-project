7b312d0a94784f9512e66336f8153d4af755a0cc
srinubasani9848-debug <srinubasani9848@gmail.com>
2025-10-16 20:26:08 +0530

    Create variables.tf

---
+++ network/variables.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "az" {
+  description = "Availability Zone"
+  type        = string
+  default     = "us-east-1a"
+}
