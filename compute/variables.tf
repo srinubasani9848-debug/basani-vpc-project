95d9ea36e02b84060e067f17f2da6aceb1866d71
srinubasani9848-debug <srinubasani9848@gmail.com>
2025-10-16 20:20:21 +0530

    Create variables.tf

---
+++ compute/variables.tf
@@ -0,0 +1,21 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "subnet_id" {
+  description = "Subnet ID for EC2"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
