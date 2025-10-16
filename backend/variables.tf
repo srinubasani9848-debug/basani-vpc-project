35c354e677687ff410cff8dcdd72871b15f11ee3
srinubasani9848-debug <srinubasani9848@gmail.com>
2025-10-16 20:18:39 +0530

    Create variables.tf

---
+++ backend/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "S3 bucket for Terraform backend"
+  type        = string
+  default     = "srinivas-terraform-backend-2025"
+}
